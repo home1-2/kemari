75b7eff568a27dc37b5781aedbc3fdd7085c3079
usaginosasimi <10yakiniku10@yahoo.ne.jp>
2022-07-30 15:50:18 +0900

    色々消した

--- docs/js/rank.js
+++ docs/js/rank.js
@@ -1,29 +1,7 @@
-// function f(){ 
-//     console.log(127);
-// }
-// function g(x) {
-//     console.log(x);
-    
-// }  
-// function kaijyooo(s) {
-//     let x=1;
-//     for (let i = 1; i <= s ; i++) {
-//          x=x*i;       
-//     }
-//     return x;
-// }
-// function startUp() {
-//   let h=$("#harada")
-//  console.log(h);  
-//  h.html("nagakura") 
-// }
-// $(startUp)
-    // let aiuuee = [12,46,74,32,46];
-    // console.log(aiuuee[0]);
+
 function loadedJson(jsonData){
     let ranktable= $("#r-t")
     for (const once of jsonData) {
-        console.log(once.name);
         ranktable.append(`<tr><td>${once.rank}</td><td>${once.name}</td><td>${once.score}</td>
                           <td>${once.time}</td></tr>`)
     }
