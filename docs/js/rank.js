7a7be7e7fb93051926d98083bda0ad9ea77d0532
usaginosasimi <10yakiniku10@yahoo.ne.jp>
2022-07-30 15:22:45 +0900

    rank

--- docs/js/rank.js
+++ docs/js/rank.js
@@ -20,5 +20,15 @@
 // $(startUp)
     // let aiuuee = [12,46,74,32,46];
     // console.log(aiuuee[0]);
+function loadedJson(jsonData){
+   let ranktable= $("#r-t")
+for (const once of jsonData) {
+    console.log(once.name);
+    ranktable.append(`<tr><td>${once.rank}</td><td>${once.name}</td><td>${once.score}</td>
+                      <td>${once.time}</td></tr>`)
+}
+}
 const URL = "https://script.google.com/macros/s/AKfycbyDXA0mt_tW6u642qXtySBH1Re_aIi938t3gLT7HqIlYG-sc3uobuFqxl3n4d9emzd82A/exec";
-console.log($.getJSON(URL));
+$.getJSON(URL,loadedJson);
+console.log(`sasas
+${12%4}asas`);
